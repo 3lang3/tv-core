0be8637996c74ddfa4d9360dcae211036be85f27
浪浪浪 <675483520@qq.com>
2017-05-20 00:34:40 +0800

    bind link

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    frontHost: 'http://121.196.207.80:8080/',
-    endHost: 'http://121.196.207.80:3000/',
-    Host: 'http://121.196.207.80',
+    frontHost: 'http://ruarua.live/',
+    endHost: 'http://ruarua.live:3000/',
+    Host: 'ruarua.live',
 }
